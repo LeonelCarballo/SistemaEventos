3d0e9bfb9fe19a3ca0497b573673b054145bff73
gaelguerra <gaelguerra05@icloud.com>
2025-05-16 16:45:21 -0700

    cambios stash
    no moví nada, no deberia afectar

--- AccesoDatos/target/generated-sources/annotations/linkup/entidades/Usuario_.java
+++ AccesoDatos/target/generated-sources/annotations/linkup/entidades/Usuario_.java
@@ -4,7 +4,11 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< Updated upstream
 @Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-05-16T16:38:34", comments="EclipseLink-2.7.12.v20230209-rNA")
+=======
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-05-14T09:20:25", comments="EclipseLink-2.7.12.v20230209-rNA")
+>>>>>>> Stashed changes
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
